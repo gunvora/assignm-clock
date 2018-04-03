952618281599354c3fdca70175e6fdefde121eae
gunvora <34217219+gunvora@users.noreply.github.com>
2018-04-03 16:39:00 +0200

    First version

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,59 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    console.log("Will Unmount")
+    clearInterval(this.timerID)
+  }
+
+  stopClock = () => {
+    clearInterval(this.timerID)
+  }
+
+  restartClock = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
 
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <div className="buttons">
+          <button onClick={this.stopClock}>
+          Pause!
+          </button>
+          <button onClick={this.restartClock}>
+          Restart!
+          </button>
+        </div>
       </div>
     )
   }
-
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById("root")
+)
